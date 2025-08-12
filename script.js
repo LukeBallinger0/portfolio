41cf765a225e5c2d8a5fa6471b1eeda3c512f311
Luke Ballinger <luke.ballinger@outlook.com>
2025-08-12 13:20:01 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const elements = document.querySelectorAll(".fade-in");
-    elements.forEach((el, index) => {
-        el.style.animationDelay = `${index * 0.2}s`;
-    });
+  const fadeElements = document.querySelectorAll(".fade-in");
+  fadeElements.forEach((el, index) => {
+    el.style.animationDelay = `${index * 0.3}s`;
+  });
 });
